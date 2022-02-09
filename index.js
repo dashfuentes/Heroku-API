2bca7de9c74118cfe47ba375fe20623773330912
dashfuentes <smis117112@gmail.com>
2022-02-08 20:22:46 -0600

    adding port

--- index.js
+++ index.js
@@ -18,9 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 require("./app/routes/movie.routes")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 8070;
-
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
